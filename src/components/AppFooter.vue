036af07fcda3e534494d2220341608bc475b540d
Marcosshyguy <thegmarcoferrari94@yahoo.it>
2022-11-22 16:23:01 +0100

    footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -12,9 +12,10 @@ export default {
 </script>
 
 <template>
-  <div class="footer-navbar">
+  <!-- footer top -->
+  <div class="footer-menu">
     <div>
-      <img src="../assets/images/logo_footer.png" alt="avada logo" />
+      <img src="../assets/images/logo_footer.png" alt="avada-logo" />
     </div>
     <div>
       <ul>
@@ -29,14 +30,27 @@ export default {
       </ul>
     </div>
   </div>
-  <div class="footer-credits"></div>
+  <!-- footer bottom -->
+
+  <div class="footer-credits">
+    <p>
+      &#169; Copyright 2012 - 2020 | AVADA THEME BY THEMEFUSIUN | ALL RIGHTS
+      RESERVED | POWERED BY WORDPRESS
+    </p>
+    <div>
+      <a href=""><i class="fa-brands fa-facebook-f"></i></a>
+      <a href=""><i class="fa-brands fa-twitter"></i></a>
+      <a href=""><i class="fa-brands fa-instagram"></i></a>
+      <a href=""><i class="fa-brands fa-youtube"></i></a>
+    </div>
+  </div>
 </template>
 
 <style lang="scss" scoped>
 @use "../style/partials/variables" as *;
 @use "../style/partials/mixins" as *;
 
-.footer-navbar {
+.footer-menu {
   background-color: $main-bg2;
   width: 100%;
   height: $hedaer-height;
@@ -61,6 +75,35 @@ export default {
       &:hover {
         opacity: 75%;
       }
+    }
+  }
+}
+.footer-credits {
+  color: $font-color1;
+  background-color: $main-bg1;
+  width: 100%;
+  height: $footer-credits;
+  padding: 0 2em;
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  a {
+    display: inline-block;
+    height: 2.3rem;
+    width: 2.3rem;
+    background-color: $main-bg2;
+    line-height: 2.3rem;
+    text-align: center;
+    margin-right: 1rem;
+
+    &:hover {
+      opacity: 75%;
+    }
+
+    &:last-child {
+      margin-right: 0;
     }
   }
 }
